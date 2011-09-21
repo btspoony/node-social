7b59be0cfff5f1869d6e17ec9699323e19984f32
Tang Bo Hao <btspoony@gmail.com>
2011-09-22 02:47:38 +0800

    modify name to createClient

--- index.js
+++ index.js
@@ -2,19 +2,19 @@
  * How to use
  * 
  *  var snsclientlib = require('snsclient');
- *  var clientFactory = snsclientlib.clientFactory,
+ *  var createClient = snsclientlib.createClient,
  * 		setDefaultAppinfo = snsclientlib.setDefaultAppinfo;
  *
- *  var client_nouser = clientFactory('wyx',{key:aa,secret:bb}); // for no authorized_user
+ *  var client_nouser = createClient('wyx',{key:aa,secret:bb}); // for no authorized_user
  *
  *  setDefaultAppinfo({key:aa, secret:bb}); 
- *  client_nouser = clientFactory('wyx'); // for no authorized_user and using default
+ *  client_nouser = createClient('wyx'); // for no authorized_user and using default
  *
- *  var client = clientFactory('wyx', authorized_user); // if set user and using default
+ *  var client = createClient('wyx', authorized_user); // if set user and using default
  */
 
 require.paths.unshift('./support');
 
 exports.getTypeByQuery = require("./lib/clientFactory").getTypeByQuery;
-exports.clientFactory = require("./lib/clientFactory").createClient;
+exports.createClient = require("./lib/clientFactory").createClient;
 exports.setDefaultAppinfo = require("./lib/clientFactory").setDefaultAppinfo;
