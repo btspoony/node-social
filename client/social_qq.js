cef3de0ab40d4ff8e4b2bf5d21d1151c8bf583e5
Tang Bo Hao <btspoony@gmail.com>
2013-01-22 12:50:45 +0800

    add sandbox

--- client/social_qq.js
+++ client/social_qq.js
@@ -7,56 +7,38 @@
 
 var social = exports.social = {};
 
-var appInfo;
-/**
- * @param options
- *    platform
- *    id // renren, wyx
- *    url // renren, wyx
- */
+var sandbox;
 social.init = function(initOpt) {
-  appInfo = initOpt;
+  sandbox = initOpt.sandbox;
 };
 
-/**
- * invite options
- * @param options
- *    msg, img
- */
 social.invite = function(options, callback) {
-  options.onSuccess = options.onSuccess || callback;
+  if( callback ) 
+    options.onSuccess = options.onSuccess || callback;
   fusion2.dialog.invite(options);
 }
 
-/**
- * @param options
- *  title
- *  summary
- *  msg
- *  img
- *  param
- *
- *  button
-          // 必须。操作区的文字，只能填写下面四种中的一种：
-          // 1.进入应用 2.领取奖励 3. 获取能量 4.帮助TA。
- */
 social.share = function(options, callback) {
-  options.onSuccess = options.onSuccess || callback;
+  if( callback ) 
+    options.onSuccess = options.onSuccess || callback;
   fusion2.dialog.sendStory(options);
 }
 
-/**
- * @param  {Object}   options  
- *         disturb - true
- *         param - url_param
- */
 social.buy = function(options, callback){
-  options.onSuccess = options.onSuccess || callback;
+  options.sandbox = sandbox;
   fusion2.dialog.buy(options);
 }
 
-social.authorize = function(redirect_uri, callback) {
-  if(callback) callback();
+social.checkBalance = function( callback ) {
+  var options = {};
+  options.onClose = callback;
+  fusion2.dialog.checkBalance(options);
+}
+
+social.recharge = function( callback ) {
+  var options = {};
+  options.onClose = callback;
+  fusion2.dialog.recharge(options);
 }
 
 })(window);
